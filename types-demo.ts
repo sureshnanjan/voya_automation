e2b712d71f91c2889285adb2e74438fb799b63bf
Suresh Trainer <sureshnanjan6@gmail.com>
2026-04-17 12:22:32 +0530

    Types Demo

--- types-demo.ts
+++ types-demo.ts
@@ -1,4 +1,7 @@
-var myname:string = "Suresh"
-var age:number = 100
-myname = 100.20
-age = "Twenty Five"
+const expected_title:string = "Welcome to Internet"
+let expected_subtitle:string = "Available Examples" 
+let  expected_example_count:number = 44;
+let banner_color = "green";
+// let var const
+expected_subtitle = "Another Value"
+//expected_example_count = 100;
