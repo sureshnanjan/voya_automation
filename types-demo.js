7cab86605b13041661f4293a2895d32d5442817c
i729502 <Deepak.Jayade@voya.com>
2026-04-22 10:22:23 +0530

    resolve the merge conflict

--- types-demo.js
+++ types-demo.js
@@ -1,7 +1,4 @@
-var expected_title = "Welcome to Internet";
-var expected_subtitle = "Available Examples";
-var expected_example_count = 44;
-var banner_color = "green";
-// let var const
-expected_subtitle = "Another Value";
-//expected_example_count = 100;
+var myname = "Suresh";
+var age = 100;
+myname = 100.20;
+age = "Twenty Five";
