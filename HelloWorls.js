b60b370189c1cf5ba0b12ab013579b47e2bfbb6d
i729502 <Deepak.Jayade@voya.com>
2026-04-27 14:32:53 +0530

    test

--- HelloWorls.js
+++ HelloWorls.js
@@ -3,7 +3,12 @@ console.log("This is my First Java Script")
 const answer = name / 100;
 console.log(answer + 100)
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 #added for gitignode
 =======
 >>>>>>> 7cab866 (resolve the merge conflict)
+=======
+
+#added for gitignode
+>>>>>>> 9b0cdc6 (gitignor settings)
